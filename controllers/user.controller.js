00ad356b9f76d7f61c6a1bb2b7ffb10bd6bbdd10
arundhi04101996@gmail.com <arundhi04101996@gmail.com>
2024-01-20 12:39:47 +0530

    edit and delete api created

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,4 @@
 import usermodel from "../models/user.model.js";
-
 import bcrypt from "bcrypt";
 import { errorHandler } from "../utils/errorhandler.js";
 
@@ -7,30 +6,36 @@ export const edituser = async (req, res, next) => {
   try {
     if (req.user._id !== req.params.id)
       next(errorHandler(400, "you can edit only your account"));
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(req.body.password, salt);
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(req.body.password, salt);
+      req.body.password = hash;
+    }
     const user = await usermodel.findByIdAndUpdate(
       req.user._id,
       {
         $set: {
           username: req.body.username,
-          password: hash,
+          password: req.body.password,
           email: req.body.email,
           photo: req.body.photo,
         },
       },
       { new: true }
     );
-    res.status(200).json(user);
+    const { password, ...rest } = user._doc;
+    res.status(200).json({ ...rest });
   } catch (error) {
     next(error);
   }
 };
 
-export const getallusers = async (req, res, next) => {
+export const deleteuser = async (req, res, next) => {
   try {
-    const user = await usermodel.find();
-    res.status(200).json(user);
+    if (req.user._id !== req.params.id)
+      return next(errorHandler(400, "you can delete only your account"));
+    const user = await usermodel.findOneAndDelete(req.user._id);
+    res.status(200).json({ message: "user deleted successfully" });
   } catch (error) {
     next(error);
   }
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { edituser, getallusers } from "../controllers/user.controller.js";
+import { edituser, deleteuser } from "../controllers/user.controller.js";
 const router = express.Router();
 import { verifyUser } from "../utils/verifyuser.js";
 
 router.put("/edituser/:id", verifyUser, edituser);
-router.get("/allusers", verifyUser, getallusers);
+router.delete("/deleteuser/:id", verifyUser, deleteuser);
 
 export default router;
