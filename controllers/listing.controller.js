33b9ef328de8158920bf022c2027818f589f8dc7
arundhi04101996@gmail.com <arundhi04101996@gmail.com>
2024-01-22 13:07:57 +0530

    api

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -30,8 +30,6 @@ export const getlist = async (req, res, next) => {
   try {
     const list = await listing.findById(req.params.id);
     if (list) {
-      if (list.userRef !== req.user._id)
-        return next(errorHandler(404, "You can get only your listing"));
       const data = await listing.findById(req.params.id);
       res.status(200).json(data);
     } else {
@@ -61,6 +59,57 @@ export const updatelisting = async (req, res, next) => {
       return next(errorHandler(404, "Listing not found!"));
     }
   } catch (error) {
+    next(error);
+  }
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 6;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["sell", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+    const lists = await listing
+      .find({
+        name: { $regex: searchTerm, $options: "i" },
+        offer,
+        furnished,
+        parking,
+        type,
+      })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+    res.status(200).json(lists);
+  } catch (error) {
+    console.log(error);
     next(error);
   }
 };
--- routes/listing.router.js
+++ routes/listing.router.js
@@ -4,13 +4,15 @@ import {
   deletelisting,
   getlist,
   updatelisting,
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyUser } from "../utils/verifyuser.js";
 const router = express.Router();
 
 router.post("/createlisting", createListing);
 router.delete("/deletelisting/:id", verifyUser, deletelisting);
-router.get("/getlist/:id", verifyUser, getlist);
+router.get("/getlist/:id", getlist);
 router.put("/updatelist/:id", verifyUser, updatelisting);
+router.get("/getlistings", verifyUser, getListings);
 
 export default router;
